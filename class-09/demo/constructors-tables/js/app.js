3487914649bdf9c4ee24fe069a7eff70a8caea44
David Souther <davidsouther+github@gmail.com>
2022-03-12 11:21:22 -0800

    Forms and events after lecture

--- class-09/demo/constructors-tables/js/app.js
+++ class-09/demo/constructors-tables/js/app.js
@@ -1,5 +1,41 @@
 "use strict";
 
+let addButton = document.getElementById("add_button");
+
+// addButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log("Clicking ", event);
+// });
+
+let musicianForm = document.getElementById("musician_form");
+musicianForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  /// same as below
+  console.log("Submitting ", event);
+
+  let form = event.target;
+
+  let first = form.first;
+  let last = form.last;
+  let nationality = form.nationality;
+
+  if (first.value == "") {
+    return;
+  }
+
+  if (nationality.value == "") {
+    return;
+  }
+
+  let musician = new Musician(first.value, last.value, nationality.value);
+  musician.render();
+
+  first.value = "";
+  last.value = "";
+  nationality.value = "";
+});
+
 let musiciansTable = document.querySelector("#musicians tbody");
 
 function Musician(first_param, last_param, nationality_param) {
